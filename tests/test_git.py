600d74b6e81290946c652f915fbfe5ab91c2fb63
Patrick Lannigan <p.lannigan@gmail.com>
2022-11-25 08:22:10 +0900

    Move git test setup to sample data

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,9 +1,6 @@
-from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional
 
 import pytest
-from git import Repo
 
 from hyper_bump_it import _git as git
 from hyper_bump_it._config import GitAction, GitActions
@@ -18,7 +15,6 @@ from hyper_bump_it._git import GitOperationsInfo
 from tests import sample_data as sd
 
 TEXT_FORMATTER = sd.some_text_formatter()
-SOME_FILE = "foo.txt"
 SOME_OTHER_FILE = "bar.txt"
 
 
@@ -43,7 +39,7 @@ def test_get_vetted_repo__not_repo__error(tmp_path: Path):
 
 
 def test_get_vetted_repo__dirty_repo__error(tmp_path: Path):
-    repo = _init_repo(tmp_path)
+    repo = sd.some_git_repo(tmp_path)
     repo.committed_file.write_text("SOME NEW TEXT")
     repo.repo.index.add([repo.committed_file])
 
@@ -52,14 +48,14 @@ def test_get_vetted_repo__dirty_repo__error(tmp_path: Path):
 
 
 def test_get_vetted_repo__detached_head__error(tmp_path: Path):
-    repo = _init_repo(tmp_path, detached=True)
+    repo = sd.some_git_repo(tmp_path, detached=True)
 
     with pytest.raises(DetachedRepositoryError):
         git.get_vetted_repo(repo.path, sd.some_git_operations_info())
 
 
 def test_get_vetted_repo__no_remote__error(tmp_path: Path):
-    repo = _init_repo(tmp_path, remote=None)
+    repo = sd.some_git_repo(tmp_path, remote=None)
 
     with pytest.raises(MissingRemoteError):
         git.get_vetted_repo(
@@ -71,7 +67,7 @@ def test_get_vetted_repo__no_remote__error(tmp_path: Path):
 
 
 def test_get_vetted_repo__existing_branch__error(tmp_path: Path):
-    repo = _init_repo(tmp_path, branch=sd.SOME_BRANCH)
+    repo = sd.some_git_repo(tmp_path, branch=sd.SOME_BRANCH)
 
     with pytest.raises(AlreadyExistsError, match="branch"):
         git.get_vetted_repo(
@@ -85,7 +81,7 @@ def test_get_vetted_repo__existing_branch__error(tmp_path: Path):
 
 
 def test_get_vetted_repo__existing_branch_no_branching__repo_returned(tmp_path: Path):
-    repo = _init_repo(tmp_path, branch=sd.SOME_BRANCH)
+    repo = sd.some_git_repo(tmp_path, branch=sd.SOME_BRANCH)
 
     result = git.get_vetted_repo(
         repo.path,
@@ -98,7 +94,7 @@ def test_get_vetted_repo__existing_branch_no_branching__repo_returned(tmp_path: 
 
 
 def test_get_vetted_repo__existing_tag__error(tmp_path: Path):
-    repo = _init_repo(tmp_path, tag=sd.SOME_TAG)
+    repo = sd.some_git_repo(tmp_path, tag=sd.SOME_TAG)
 
     with pytest.raises(AlreadyExistsError, match="tag"):
         git.get_vetted_repo(
@@ -114,7 +110,7 @@ def test_get_vetted_repo__existing_tag__error(tmp_path: Path):
 
 
 def test_get_vetted_repo__existing_tag_no_tagging__repo_returned(tmp_path: Path):
-    repo = _init_repo(tmp_path, tag=sd.SOME_TAG)
+    repo = sd.some_git_repo(tmp_path, tag=sd.SOME_TAG)
 
     result = git.get_vetted_repo(
         repo.path,
@@ -130,7 +126,7 @@ def test_get_vetted_repo__existing_tag_no_tagging__repo_returned(tmp_path: Path)
 
 
 def test_create_branch__result_has_name(tmp_path: Path):
-    repo = _init_repo(tmp_path)
+    repo = sd.some_git_repo(tmp_path)
 
     git.create_branch(repo.repo, sd.SOME_BRANCH)
 
@@ -138,7 +134,7 @@ def test_create_branch__result_has_name(tmp_path: Path):
 
 
 def test_switch_to__active_branch_is_expected(tmp_path: Path):
-    repo = _init_repo(tmp_path, branch=sd.SOME_BRANCH)
+    repo = sd.some_git_repo(tmp_path, branch=sd.SOME_BRANCH)
 
     git.switch_to(repo.repo, sd.SOME_BRANCH)
 
@@ -146,18 +142,21 @@ def test_switch_to__active_branch_is_expected(tmp_path: Path):
 
 
 def test_commit_change__edited_file__in_commit(tmp_path: Path):
-    repo = _init_repo(tmp_path)
+    repo = sd.some_git_repo(tmp_path)
     repo.committed_file.write_text("SOME NEW TEXT")
 
     git.commit_changes(repo.repo, sd.SOME_COMMIT_MESSAGE)
 
     commit_diff = repo.repo.head.commit.diff("HEAD^")
     assert len(commit_diff) == 1
-    assert commit_diff[0].a_path == SOME_FILE and commit_diff[0].change_type == "M"
+    assert (
+        commit_diff[0].a_path == sd.SOME_GLOB_MATCHED_FILE_NAME
+        and commit_diff[0].change_type == "M"
+    )
 
 
 def test_commit_change__edited_file__message_from_pattern(tmp_path: Path):
-    repo = _init_repo(tmp_path)
+    repo = sd.some_git_repo(tmp_path)
     repo.committed_file.write_text("SOME NEW TEXT")
 
     git.commit_changes(repo.repo, sd.SOME_COMMIT_MESSAGE)
@@ -167,18 +166,21 @@ def test_commit_change__edited_file__message_from_pattern(tmp_path: Path):
 
 
 def test_commit_change__deleted_file__in_commit(tmp_path: Path):
-    repo = _init_repo(tmp_path)
+    repo = sd.some_git_repo(tmp_path)
     repo.committed_file.unlink()
 
     git.commit_changes(repo.repo, sd.SOME_COMMIT_MESSAGE)
 
     commit_diff = repo.repo.head.commit.diff("HEAD^")
     assert len(commit_diff) == 1
-    assert commit_diff[0].a_path == SOME_FILE and commit_diff[0].new_file
+    assert (
+        commit_diff[0].a_path == sd.SOME_GLOB_MATCHED_FILE_NAME
+        and commit_diff[0].new_file
+    )
 
 
 def test_commit_change__new_file__in_commit(tmp_path: Path):
-    repo = _init_repo(tmp_path)
+    repo = sd.some_git_repo(tmp_path)
     repo.path.joinpath(SOME_OTHER_FILE).write_text("")
 
     git.commit_changes(repo.repo, sd.SOME_COMMIT_MESSAGE)
@@ -189,7 +191,7 @@ def test_commit_change__new_file__in_commit(tmp_path: Path):
 
 
 def test_create_tag__repo_tagged_head_commit(tmp_path: Path):
-    repo = _init_repo(tmp_path)
+    repo = sd.some_git_repo(tmp_path)
 
     git.create_tag(repo.repo, sd.SOME_TAG)
 
@@ -198,7 +200,7 @@ def test_create_tag__repo_tagged_head_commit(tmp_path: Path):
 
 
 def test_push_changes__no_branch_no_tag__only_commit_pushed(tmp_path: Path):
-    repo = _init_repo(tmp_path, remote=sd.SOME_REMOTE)
+    repo = sd.some_git_repo(tmp_path, remote=sd.SOME_REMOTE)
 
     git.push_changes(
         repo.repo,
@@ -213,7 +215,7 @@ def test_push_changes__no_branch_no_tag__only_commit_pushed(tmp_path: Path):
 
 
 def test_push_changes__tag_no_push__commit_pushed_but_not_tag(tmp_path: Path):
-    repo = _init_repo(tmp_path, remote=sd.SOME_REMOTE)
+    repo = sd.some_git_repo(tmp_path, remote=sd.SOME_REMOTE)
     repo.repo.create_tag(sd.SOME_TAG)
 
     git.push_changes(
@@ -228,7 +230,7 @@ def test_push_changes__tag_no_push__commit_pushed_but_not_tag(tmp_path: Path):
 
 
 def test_push_changes__tag_and_push__commit_and_tag_pushed(tmp_path: Path):
-    repo = _init_repo(tmp_path, remote=sd.SOME_REMOTE)
+    repo = sd.some_git_repo(tmp_path, remote=sd.SOME_REMOTE)
     repo.repo.create_tag(sd.SOME_TAG)
 
     git.push_changes(
@@ -243,60 +245,6 @@ def test_push_changes__tag_and_push__commit_and_tag_pushed(tmp_path: Path):
     assert repo.remote_repo.active_branch.commit == repo.repo.active_branch.commit
     assert sd.SOME_TAG in repo.remote_repo.tags
     assert repo.repo.tags[sd.SOME_TAG].commit == repo.repo.active_branch.commit
-
-
-@dataclass
-class InitedRepo:
-    repo: Repo
-    path: Path
-    remote_repo: Repo
-    committed_file: Path
-
-
-def _init_repo(
-    test_root: Path,
-    remote: Optional[str] = None,
-    branch: Optional[str] = None,
-    tag: Optional[str] = None,
-    detached: bool = False,
-) -> InitedRepo:
-    """
-    Initialize test repository setup (a primary with a single commit and remote with no commits)
-
-    :param test_root: Directory to store repositories.
-    :param remote: Name to use when referencing remote repo. If `None` the remote repo will not be
-        linked to the primary_repo.
-    :param branch: Name to use for an additional branch created on the primary repository. If
-        `None`, no branch will be created.
-    :param tag: Name to use for a tag created on the primary repository. If `None`, no tag will be
-        created.
-    :param detached: If `True`, switch from the default branch to a detached HEAD situation that
-        only points to the initial commit.
-    :return: Data about the initialized repos.
-    """
-    repo_dir = test_root / "primary"
-    repo = Repo.init(repo_dir, mkdir=True)
-    remote_path = test_root / "other"
-    remote_repo = Repo.init(test_root / "other", mkdir=True, bare=True)
-
-    file = repo_dir / SOME_FILE
-    file.touch()
-    repo.index.add([file])
-    repo.index.commit("a first commit")
-
-    if remote is not None:
-        repo.create_remote(remote, str(remote_path))
-
-    if branch is not None:
-        repo.create_head(branch)
-
-    if tag is not None:
-        repo.create_tag(tag)
-
-    if detached:
-        repo.head.reference = repo.commit("HEAD")
-
-    return InitedRepo(repo, repo_dir, remote_repo, file)
 
 
 class FakeException(Exception):
