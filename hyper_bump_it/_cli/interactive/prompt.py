ced1edb32864e25e9205c90fdb639e66f32c3e06
Patrick Lannigan <plannigan@gmail.com>
2023-01-07 14:42:18 -0500

    Extract out functionality to list options

--- hyper_bump_it/_cli/interactive/prompt.py
+++ hyper_bump_it/_cli/interactive/prompt.py
@@ -1,8 +1,9 @@
 """
 Helper functionality for interactive prompts.
 """
+from collections.abc import Mapping
 from enum import Enum
-from typing import TypeVar, cast
+from typing import Optional, TypeVar, cast
 
 from rich.prompt import Prompt, PromptBase
 from rich.text import Text
@@ -13,24 +14,25 @@ EnumT = TypeVar("EnumT", bound=Enum)
 
 def enum_prompt(
     message: str,
-    option_descriptions: dict[EnumT, str],
+    option_descriptions: Mapping[EnumT, str],
     default: EnumT,
 ) -> EnumT:
     enum_type = type(default)
     if set(enum_type) != option_descriptions.keys():
         missing_values: set[EnumT] = set(enum_type) - option_descriptions.keys()
         raise ValueError(
-            f"Missing description for: {','.join(x.value for x in missing_values)}"
+            f"Missing description for: {','.join(cast(str,x.value) for x in missing_values)}"
         )
     prompt_text = Text(message)
     prompt_text.append("\n")
-    for option, description in option_descriptions.items():
-        prompt_text.append(cast(str, option.value), style="prompt.choices")
-        prompt_text.append(f" - {description}")
-        if option == default:
-            prompt_text.append(" ")
-            prompt_text.append("(default)", style="prompt.default")
-        prompt_text.append("\n")
+    list_options(
+        prompt_text,
+        {
+            option.value: description
+            for option, description in option_descriptions.items()
+        },
+        cast(str, default.value),
+    )
     prompt_text.append("Enter the option name")
     result_value = Prompt.ask(
         prompt_text,
@@ -40,6 +42,25 @@ def enum_prompt(
         default=default.value,
     )
     return enum_type(result_value)
+
+
+def list_options(
+    text: Text, option_descriptions: Mapping[str, str], default: Optional[str] = None
+) -> None:
+    """
+    Write the list of options and descriptions to the given text instance.
+
+    :param text: Object to aggregate the text to be displayed
+    :param option_descriptions: A set of options that can be selected paired with a description.
+    :param default: Option that should include a default label. If `None`, no label will be added.
+    """
+    for option, description in option_descriptions.items():
+        text.append(option, style="prompt.choices")
+        text.append(f" - {description}")
+        if option == default:
+            text.append(" ")
+            text.append("(default)", style="prompt.default")
+        text.append("\n")
 
 
 class VersionPrompt(PromptBase[Version]):
