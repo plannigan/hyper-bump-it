f35cb4fc05362c7cb4f0e4711bb227673cf97007
Patrick Lannigan <p.lannigan@gmail.com>
2023-01-15 14:47:04 -0500

    Correct script import path
    Add CI step to execute script

--- .github/actions/verify-wheel/action.sh
+++ .github/actions/verify-wheel/action.sh
@@ -2,6 +2,7 @@ set -euo pipefail
 
 MODULE_NAME=$1
 WHEEL_LOCATION=$2
+SCRIPT_COMMAND=$3
 
 echo "Ensuring pip is up to date"
 python -m pip install --upgrade pip
@@ -18,3 +19,10 @@ pip install ${APP_DIR}/${WHEEL_LOCATION}/*.whl
 echo "-----------------------------"
 echo "Attempting to import package"
 python "${GITHUB_ACTION_PATH}/test_module_import.py" "${MODULE_NAME}"
+
+if [[ "${SCRIPT_COMMAND}" != "" ]]; then
+    echo "-----------------------------"
+    echo "Attempting to run script"
+    echo "${SCRIPT_COMMAND}"
+    ${SCRIPT_COMMAND}
+fi
